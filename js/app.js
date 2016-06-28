5f672fc19b2c5d14bfd1e5a4c7d41c40736039f6
Vincent <vincentnewkirk@gmail.com>
2016-06-27 19:36:43 -1000

    step 4 completed

--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
-window.onload = function(){
-  var appList = new ShoppingList();
-  var divContent = document.getElementById('content');
+var appList = new ShoppingList();
+var divContent = document.getElementById('content');
 
-  function add_to_shopping_list(event){
-    var itemName = document.getElementById('title').value;
-    var itemDesc = document.getElementById('description').value;
-    var new_shopping_list_item = new ShoppingListItem(itemName, itemDesc);
-    event.preventDefault();
+function add_to_shopping_list(event){
+  var itemName = document.getElementById('title').value;
+  var itemDesc = document.getElementById('description').value;
+  var new_shopping_list_item = new ShoppingListItem(itemName, itemDesc);
+  event.preventDefault();
 
-    appList.addItem(new_shopping_list_item);
-    divContent.innerHTML = appList.render();
+  appList.addItem(new_shopping_list_item);
+  divContent.innerHTML = appList.render();
+}
+document.getElementById('add_shopping_list_item_button').addEventListener('click', add_to_shopping_list);
+
+function changeCheckedStatus(idx, checkbox){
+    if( checkbox.checked ){
+      appList.items[idx].check();
+  } else {
+    appList.items[idx].uncheck();
   }
-  document.getElementById('add_shopping_list_item_button').addEventListener('click', add_to_shopping_list);
+}
 
-};
-  function changeCheckedStatus(index, checkbox){
-    console.log('hi', index, checkbox);
-  }
